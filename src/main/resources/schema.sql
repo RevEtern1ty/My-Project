0c6ffafdeb2a4c7c145a2f5019608447e25c84c7
QBIT PC <bsv.ttu@gmailcom>
2025-12-16 22:23:21 +0200

    initial commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+-- ******************************************************************
+-- PLACEHOLDER SCHEMA (blank project)
+-- Remove these lines and replace with real DDL scripts when ready
+-- ******************************************************************
+CREATE TABLE placeholder_dummy (id INT);
+DROP TABLE placeholder_dummy;
