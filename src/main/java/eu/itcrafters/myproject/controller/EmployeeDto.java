8a032b292b96b8ddde7a555cc8ff205a6b794db5
QBIT PC <bsv.ttu@gmailcom>
2025-12-17 19:16:50 +0200

    Improving Dto

--- src/main/java/eu/itcrafters/myproject/controller/EmployeeDto.java
+++ src/main/java/eu/itcrafters/myproject/controller/EmployeeDto.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Value;
+
 
 import java.io.Serializable;
 
