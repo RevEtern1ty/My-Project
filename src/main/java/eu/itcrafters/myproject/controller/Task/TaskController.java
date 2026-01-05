b351e6f04a8ca601e989160bec187eecba0c9e13
Vladislav Borissov <bsv.ttu@gmailcom>
2026-01-05 20:41:48 +0200

    feat: add Task API layer

--- src/main/java/eu/itcrafters/myproject/controller/Task/TaskController.java
+++ src/main/java/eu/itcrafters/myproject/controller/Task/TaskController.java
@@ -1,18 +1,41 @@
 package eu.itcrafters.myproject.controller.Task;
 
+import eu.itcrafters.myproject.controller.Task.dto.TaskCreateRequest;
+import eu.itcrafters.myproject.controller.Task.dto.TaskDto;
+import eu.itcrafters.myproject.controller.Task.dto.TaskUpdateRequest;
 import eu.itcrafters.myproject.service.Task.TaskService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/tasks")
 public class TaskController {
 
     private final TaskService taskService;
 
-    @GetMapping("/tasks")
-    public void getAllTasks(){
-        taskService.getAllTasks();
+    @GetMapping("/all")
+    public List<TaskDto> getAllTasks(){
+        return taskService.getAllTasks();
     }
+
+    @GetMapping("/gettask/{id}")
+    public TaskDto getTask(@PathVariable Long id){
+        return taskService.getTask(id);
+    }
+
+    @PostMapping
+    public TaskDto createTask(@Valid @RequestBody TaskCreateRequest request){
+        return taskService.createTask(request);
+    }
+
+    @PatchMapping("/update/{id}")
+    public TaskDto updateTask (@PathVariable Long id, @RequestBody TaskUpdateRequest request){
+        return taskService.updateTask(id,request);
+    }
+
+
 }
