3109ca8f44cefdc08f87fe7e757d087e6003201d
Vladislav Borissov <bsv.ttu@gmailcom>
2026-01-06 12:08:19 +0200

    feat: add Workstation API layer

--- src/main/java/eu/itcrafters/myproject/controller/Workstation/WorkstationController.java
+++ src/main/java/eu/itcrafters/myproject/controller/Workstation/WorkstationController.java
@@ -1,11 +1,14 @@
 package eu.itcrafters.myproject.controller.Workstation;
 
+import eu.itcrafters.myproject.controller.Workstation.dto.WorkstationCreateRequest;
+import eu.itcrafters.myproject.controller.Workstation.dto.WorkstationDto;
+import eu.itcrafters.myproject.controller.Workstation.dto.WorkstationUpdateRequest;
 import eu.itcrafters.myproject.service.Workstation.WorkstationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,13 +18,28 @@ public class WorkstationController {
     private final WorkstationService workstationService;
 
 
-    @GetMapping
-    public void  getAll() {
-        workstationService.getAllWorkstations();
+    @GetMapping("/all")
+    public List<WorkstationDto> getAllWorkstations() {
+        return workstationService.getAllWorkstations();
     }
 
     @GetMapping("/{id}")
-    public void getById(@PathVariable Long id) {
-        workstationService.getWorkstationById(id);
+    public WorkstationDto getWorkstation(@PathVariable Long id) {
+        return workstationService.getWorkstationById(id);
+    }
+
+    @PostMapping("/create")
+    public WorkstationDto createWorkstation(@Valid @RequestBody WorkstationCreateRequest request) {
+        return workstationService.createWorkstation(request);
+    }
+
+    @PatchMapping("/update/{id}")
+    public WorkstationDto updateWorkstation(@PathVariable Long id, @RequestBody WorkstationUpdateRequest request) {
+        return workstationService.updateWorkstation(id, request);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteWorkstation(@PathVariable Long id) {
+        workstationService.deleteWorkstation(id);
     }
 }
